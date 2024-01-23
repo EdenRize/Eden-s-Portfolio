ad1c8a7cb6914853376a2350554bc8c135b247d7
EdenRize <edenrize@gmail.com>
2024-01-23 17:55:46 +0200

    add for-table-layout

--- src/cmps/SocialLink.jsx
+++ src/cmps/SocialLink.jsx
@@ -1,7 +1,7 @@
 export function SocialLink({ name, imgUrl, link }) {
     return (
-        <a href={link} target="_blank" className={`${name} social-link`}>
-            <div className="flex justify-center align-center icon-container">
+        <a href={link} target="_blank" className="social-link">
+            <div className={`${name} flex justify-center align-center icon-container`}>
                 <div className="flex justify-center align-center img-container">
                     <img src={imgUrl} />
                 </div>
