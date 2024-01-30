a546caba1dbf0eeb8b0fab7eafa521acc0caccac
EdenRize <edenrize@gmail.com>
2024-01-30 18:51:13 +0200

    Add h1

---
+++ src/cmps/Projects.jsx
@@ -0,0 +1,13 @@
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+
+export function Projects() {
+
+    return (
+        <div className="projects">
+            <h1 className="font-Gilmer-Outline">Featured Projects</h1>
+
+        </div>
+    );
+}
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Hero } from "../cmps/Hero";
+import { Projects } from "../cmps/Projects";
 
 export function HomePage() {
     return (
         <section className="home-page">
             <Hero />
+            <Projects />
         </section>
     )
 }
